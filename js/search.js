1d2b54d7002227dadd36acc643827be3fd5ff9b5
Mohammed Jhangir Dastgir <u1457103@hud.ac.uk>
2017-03-30 19:40:48 +0100

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -3,13 +3,13 @@ $('#searchsingers').keyup(function() {
  var searchTerm = $(this).val(); 
  var myExp = new RegExp(searchTerm, "i"); 
 
- $.getJSON('data/lyrics.json', function(data){
+ $.getJSON('../data/lyrics.json', function(data){
 
   var output = "<ul id='result'>";
   $.each(data, function(key, val){
    if(val.title.search(myExp) != -1){
     output += '<li>';
-    output += '<a class="tab-item" href="../html/lyrics.html" data-ignore="push">' + val.title + '</a>';
+    output += '<a class="tab-item" href="/html/lyrics.html" data-ignore="push">' + val.title + '</a>';
     output += '</li>';
    }
   });//end each
