6fc89ce0a6cc6b156ecf49c9c316a2630d480f8b
Mohammed Jhangir Dastgir <u1457103@hud.ac.uk>
2017-03-30 19:33:24 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
- $.getJSON('data/lyrics.json', function(data){
+ $.getJSON('../data/lyrics.json', function(data){
     var html = "<ul id='result'>";
       $.each(data, function (key, val) {
       html += '<img src ="' + val.image_url + '" class="image-styles" width="64" height="64" />';
       html += '<p class="image-title">' + val.title + '</p>';
-      html += '<a class="tab-item" href="../html/viewlyrics.html" data-ignore="push">' + val.url + '</a>';
+      html += '<a class="tab-item" href="html/viewlyrics.html" data-ignore="push">' + val.url + '</a>';
       html += '<p class="image-description">' + val.description + '</p>';
       });
       html += "</ul>";
